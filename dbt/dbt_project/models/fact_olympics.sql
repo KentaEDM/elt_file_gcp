e4a9844b6d82fefde8d4af1b7ab365ca978db914
Kenta <kentaedmonda01@gmail.com>
2024-09-15 19:02:49 +0700

    integrate the transformation process using DBT

---
+++ dbt/dbt_project/models/fact_olympics.sql
@@ -0,0 +1,51 @@
+WITH athletes AS (
+SELECT
+    code as athletes_id,
+    name,
+    name_tv,
+    gender,
+    function as sport_role,
+    country_code,
+    country,
+    nationality,
+    height,
+    weight,
+    birth_date,
+    DATE_DIFF(CURRENT_DATE(), birth_date, YEAR) as age,
+    education,
+    ROW_NUMBER() OVER (PARTITION BY code) as row_num
+
+FROM {{source('edm_data','table_athletes')}}
+),
+
+medals AS(
+SELECT
+    CAST(REGEXP_EXTRACT(code, r'\d+') AS INT64) as medal_code,
+    medal_type,
+    medal_date,
+    name,
+    discipline,
+    event,
+    ROW_NUMBER() OVER (PARTITION BY code) as row_num_mdl
+FROM {{source('edm_data','table_medals')}}
+WHERE REGEXP_CONTAINS(code, r'\d+') 
+)
+
+SELECT 
+    DISTINCT
+    ath.athletes_id,
+    ath.name,
+    ath.name_tv,
+    ath.gender,
+    ath.sport_role,
+    ath.country_code,
+    ath.country,
+    ath.nationality,
+    ath.height,
+    ath.weight,
+    ath.birth_date,
+    ath.age,
+    mdl.medal_type
+FROM athletes as ath 
+LEFT JOIN medals as mdl on ath.athletes_id = mdl.medal_code 
+WHERE row_num = 1 
